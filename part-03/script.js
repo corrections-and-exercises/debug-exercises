80c9836aeafb5a6f76039942265836bb211bfb85
patrick-plue <patrick.pluequett@wbscodingschool.com>
2024-09-04 13:53:23 +0200

    add solution for node-exercise

---
+++ part-03/script.js
@@ -0,0 +1,45 @@
+//setup
+const choices = ['scissors', 'rock', 'scissors'];
+
+//input
+let userInput = process.argv[2]?.toLowerCase();
+
+//steps
+validateInput(userInput, choices);
+
+const computerChoice = generateComputerChoice(choices);
+
+determineWinner(userInput, computerChoice);
+
+//functions
+function validateInput(input, options) {
+    if (!input) {
+        console.log('Please provide your choice!');
+        process.exit();
+    }
+    if (!options.includes(input)) {
+        console.log('Not a valid input!');
+        process.exit();
+    }
+}
+
+function generateComputerChoice(options) {
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+function determineWinner(userChoice, computerChoice) {
+    console.log(
+        `You chose ${userInput} and the computer chose ${computerChoice}.`
+    );
+    if (userChoice === computerChoice) {
+        console.log("It's a tie");
+    } else if (
+        (userChoice === 'rock' && computerChoice === 'scissors') ||
+        (userChoice === 'paper' && computerChoice === 'rock') ||
+        (userChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        console.log('You win!');
+    } else {
+        console.log('Computer wins!');
+    }
+}
